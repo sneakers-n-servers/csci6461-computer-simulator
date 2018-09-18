66359769ad9b47e47969b6c6b47d1a57b4479f02
sneakers-n-servers <m.afrank@yahoo.com>
2018-09-18 15:41:04 -0400

    Added example logger

--- src/main/java/edu/gw/csci/simulator/App.java
+++ src/main/java/edu/gw/csci/simulator/App.java
@@ -3,10 +3,15 @@ package edu.gw.csci.simulator;
 import edu.gw.csci.simulator.gui.Simulator;
 import edu.gw.csci.simulator.memory.Memory;
 import javafx.application.Application;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class App {
 
+    private static final Logger logger = LogManager.getLogger(App.class);
+
     public static void main(String [] args){
+        logger.info("Starting application");
         Application.launch(Simulator.class, args);
     }
 
