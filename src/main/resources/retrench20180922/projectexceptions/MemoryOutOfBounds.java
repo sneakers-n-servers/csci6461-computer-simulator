3157bd6e87fe264e3b6e95ffa075eaa04d74e06c
heidiklum <heidi@klum.com>
2018-09-22 16:26:38 +0000

    adding exception handlers since i don't know how to connect to the repo's

---
+++ src/main/resources/retrench20180922/projectexceptions/MemoryOutOfBounds.java
@@ -0,0 +1,35 @@
+/*
+Project 1 - Exception : MemoryOutOfBounds
+
+%%%%%    %%%%%%%%%%    %%%%%%%%%%    %%%%%
+%%%%%    Revision:    20180916
+%%%%%    %%%%%%%%%%    %%%%%%%%%%    %%%%%
+*/
+
+/*
+%%%%%%%%%%
+REVISION HISTORY
+
+%%%     20180916
+        Modified:   
+    -
+
+%%%     20180916
+        Author: Group 9    
+        Project:    CSCI_6461_F18_Project1
+    -Initial release
+
+%%%%%%%%%%
+*/
+
+//PACKAGE MEMBERSHIP
+package projectexceptions;
+
+public class MemoryOutOfBounds extends Exception {
+  //CONSTRUCTORS
+  public MemoryOutOfBounds(String exception_string) {
+    // Call constructor of parent Exception
+    super(exception_string);
+  }
+}
+
