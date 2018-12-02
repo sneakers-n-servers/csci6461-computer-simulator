51dd3f2b5efaa905e13bd6cc00ecfb7aa5798f41
heidi klum <naim@gwu.edu>
2018-12-02 00:18:44 +0000

    shuffled memory

--- src/main/java/edu/gw/csci/simulator/gui/PreStoreMemory.java
+++ src/main/java/edu/gw/csci/simulator/gui/PreStoreMemory.java
@@ -27,35 +27,35 @@ public class PreStoreMemory {
      * @param cpu
      */
     public static void PreStoreMemoryForProgram3(CPU cpu) {
-      // this currently assumes memory location 7 for the first instruction - see  SetProgram3
-        cpu.StoreValue(25, 3);    // int for float
-        cpu.StoreValue(26, 4);    // int for float
-        cpu.StoreValue(29, 10);    // vectors length
-        cpu.StoreValue(30, 5);    // vector 1 [0]
-        cpu.StoreValue(31, 5);    //
-        cpu.StoreValue(32, 5);    //
-        cpu.StoreValue(33, 5);    //
-        cpu.StoreValue(34, 5);    //
-        cpu.StoreValue(35, 5);    //
-        cpu.StoreValue(36, 5);    //
-        cpu.StoreValue(37, 5);    //
-        cpu.StoreValue(38, 5);    //
-        cpu.StoreValue(39, 5);    //
-        cpu.StoreValue(40, 10);    // vector 2 [0]
-        cpu.StoreValue(41, 10);    //
-        cpu.StoreValue(42, 10);    //
-        cpu.StoreValue(43, 10);    //
-        cpu.StoreValue(44, 10);    //
-        cpu.StoreValue(45, 10);    //
-        cpu.StoreValue(46, 10);    //
-        cpu.StoreValue(47, 10);    //
-        cpu.StoreValue(48, 10);    //
-        cpu.StoreValue(49, 10);    //
-        cpu.StoreValue(54, 0);    // temp
-        cpu.StoreValue(55, 7);    // *(program) - change only on fractional shift
-        cpu.StoreValue(56, 25);    // *(ints for float)
-        cpu.StoreValue(57, 29);    // *(vectors length)
-        cpu.StoreValue(58, 30);    // vector 1 address
-        cpu.StoreValue(59, 40);    // vector 2 address
+        // this currently assumes memory location 7 for the first instruction - see  SetProgram3
+        cpu.StoreValue(21, 0);    // temp
+        cpu.StoreValue(22, 7);    // *(program) - change only on fractional shift
+        cpu.StoreValue(23, 27);    // *(ints for float)
+        cpu.StoreValue(24, 38);    // *(vectors length)
+        cpu.StoreValue(25, 39);    // vector 1 address
+        cpu.StoreValue(26, 49);    // vector 2 address
+        cpu.StoreValue(27, 3);    // int for float
+        cpu.StoreValue(28, 4);    // int for float
+        cpu.StoreValue(38, 10);    // vectors length
+        cpu.StoreValue(39, 5);    // vector 1 [0]
+        cpu.StoreValue(40, 5);    //
+        cpu.StoreValue(41, 5);    //
+        cpu.StoreValue(42, 5);    //
+        cpu.StoreValue(43, 5);    //
+        cpu.StoreValue(44, 5);    //
+        cpu.StoreValue(45, 5);    //
+        cpu.StoreValue(46, 5);    //
+        cpu.StoreValue(47, 5);    //
+        cpu.StoreValue(48, 5);    //
+        cpu.StoreValue(49, 10);    // vector 2 [0]
+        cpu.StoreValue(50, 10);    //
+        cpu.StoreValue(51, 10);    //
+        cpu.StoreValue(52, 10);    //
+        cpu.StoreValue(53, 10);    //
+        cpu.StoreValue(54, 10);    //
+        cpu.StoreValue(55, 10);    //
+        cpu.StoreValue(56, 10);    //
+        cpu.StoreValue(57, 10);    //
+        cpu.StoreValue(58, 10);    //
     }
 }
